adb0e84fee9174ed19cf38e99eaca67b218267b8
Hannah Gamiel <hgamiel15@my.whitworth.edu>
2015-04-13 23:45:50 -0700

    Just got navigation controller to push on dynamically created view controllers, and pop them off when "back" is pressed. I'm currently having trouble sending the new URL to the next view controller, however.

--- urec_ios/urec_ios/FacilityViewController.swift
+++ urec_ios/urec_ios/FacilityViewController.swift
@@ -8,24 +8,94 @@
 
 import UIKit
 
-class FacilityViewController: UIViewController {
+class FacilityViewController: UIViewController, UIWebViewDelegate {
     
     @IBOutlet var webView: UIWebView!
+    @IBOutlet var activity: UIActivityIndicatorView!
+    @IBOutlet var navigationBar: UINavigationItem!
+    //var newVC: FacilityViewController!
+    var firstTime = true
+    var url : NSString = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = NSURL(string: "http://hannahgamiel.com")
-        let request = NSURLRequest(URL: url!)
+        
+        if(firstTime) {
+            println("FIRST TIME!")
+            if(url == "") {
+                url = "http://hannahgamiel.com"
+            }
+            else {
+                println(url);
+            }
+        }
+        
+        setInitialWebView()
+
+        //navigationController?.pushViewController(newVC, animated: true)
+ 
+    }
+    
+    func setInitialWebView() {
+        let request = NSURLRequest(URL: (NSURL(string: url))!)
+        webView.delegate = self
         webView.loadRequest(request)
         webView.scalesPageToFit = true
         webView.frame=self.view.bounds
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    func webViewDidStartLoad(webView: UIWebView){
+        startAnimating()
+        if(!firstTime) {
+            
+            let newURL : NSString = (webView.request?.URL.absoluteString)!
+            webView.stopLoading();
+            
+            let newVC = self.storyboard?.instantiateViewControllerWithIdentifier("Facility") as FacilityViewController
 
+            //let newVC = FacilityViewController(nibName: "FacilityViewController", bundle: nil)
+            
+            newVC.title = "Testing"
+            newVC.url = newURL
+            
+            self.navigationController?.pushViewController(newVC, animated: true)
+            
+            stopAnimating()
 
+        }
+    }
+    
+    func goBack() {
+        navigationController?.popViewControllerAnimated(true);
+        
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView){
+        if(firstTime) {
+            firstTime = false
+            //navigationController?.pushViewController(newVC, animated: true)
+        }
+        stopAnimating()
+        
+    }
+    
+    func startAnimating(){
+        activity.startAnimating()
+        activity.hidesWhenStopped = true
+    }
+    
+    func stopAnimating(){
+        activity.stopAnimating()
+        activity.hidesWhenStopped = true
+        
+    }
 }
 
