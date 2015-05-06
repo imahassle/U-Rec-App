234a7c7ad35e47078261f25082fe93686dc1848b
Joey Corry <jcorry.git@gmail.com>
2015-05-05 19:33:57 -0700

    Removed old CMS folder.

--- CMS/js/views/loading.js
+++
@@ -1,9 +0,0 @@
-app.views.load = Backbone.View.extend({
-	template: _.template($("#loading").html()),
-	initialize: function() {
-		this.render();
-	},
-	render: function() {
-		this.$el.html(this.template({}));
-	}
-});
